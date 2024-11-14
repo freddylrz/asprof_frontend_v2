262df8cd76c013811b97041e2b4b286e3479f99a
freddylrz <freddy@tib.co.id>
2024-11-14 16:03:10 +0700

    update controller message

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -5,22 +5,32 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Events\MessageSent;
+use Exception;
 
 class MessageController extends Controller
 {
     public function index(Request $r)
     {   
-        $arrayData = [
-            'room_id'    => $r->room_id,
-            'user_id'    => 1,
-            'message'    => $r->message,
-            'created_at' => now(),
-        ];
+        try
+        {            
+            $arrayData = [
+                'room_id'    => $r->room_id,
+                'user_id'    => 1,
+                'message'    => $r->message,
+                'created_at' => now(),
+            ];
 
-        event(new MessageSent($r->room_id, $arrayData));
+            event(new MessageSent($r->room_id, $arrayData));
 
-        return [
-            'status' => 200
-        ];
+            return [
+                'status' => 200
+            ];
+        }        
+        catch (Exception $e) 
+        {
+            return [
+                'status' => 500
+            ];
+        }
     }
 }

2fd4dacb835ca0fb829e2ec1bc3b3cfe2c02e313
freddylrz <freddy@tib.co.id>
2024-11-18 17:11:16 +0700

    fix error

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Events\MessageSent;
+use App\Events\MessageCount;
 use Exception;
 
 class MessageController extends Controller

2db82b8fef5bae76871f60cf571211bb7231a923
Firstio Anmar <48433601+firstioanmar@users.noreply.github.com>
2024-12-05 11:23:16 +0700

    bug fixing chat on dekstop

--- resources/views/v1/layouts/landing.blade.php
+++ resources/views/v1/layouts/landing.blade.php
@@ -767,8 +767,7 @@
       </div>
       <style>
         #chat-box {
-            display: none; /* Initially hidden */
-            position: absolute;
+            position: fixed;
             bottom: 80px; /* Adjusted to appear above the toggle button */
             right: 20px;
             z-index: 1000;

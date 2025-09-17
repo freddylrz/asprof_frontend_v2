c0ef1f6e639c0ebae57ac1632743d23dc1bab246
Firstio Anmar <48433601+firstioanmar@users.noreply.github.com>
2025-09-17 19:30:16 +0700

    update laporan klaim

--- resources/views/v1/dashboard/klaim/data.blade.php
+++ resources/views/v1/dashboard/klaim/data.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-12">
           <div class="card">
             <div class="card-header">
-              <h3>Daftar Klaim</h3>
+              <h3>Laporan Klaim</h3>
             </div>
             <div class="card-body">
               <div class="table-responsive dt-responsive">
